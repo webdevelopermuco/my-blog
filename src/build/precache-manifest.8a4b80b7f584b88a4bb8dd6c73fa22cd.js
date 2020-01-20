d1c2aa8b70020257f52628ba75c5b28828477711
muco <mucahit@webcobi.com>
2020-01-20 12:35:05 +0300

    initial commit

---
+++ src/build/precache-manifest.8a4b80b7f584b88a4bb8dd6c73fa22cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a562e1d080901fc8e52ef3fe069ca3fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bb8da67db9238dc0e38",
+    "url": "/static/css/main.a6f2b588.chunk.css"
+  },
+  {
+    "revision": "0ca39278cd7385909cd1",
+    "url": "/static/js/2.b8786736.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.b8786736.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4bb8da67db9238dc0e38",
+    "url": "/static/js/main.e65a4f6b.chunk.js"
+  },
+  {
+    "revision": "c59d8ddbc435ff2fc636",
+    "url": "/static/js/runtime-main.2bac53c8.js"
+  }
+]);
